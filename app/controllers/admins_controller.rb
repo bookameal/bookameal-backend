a26d875c4dcf16a71c249bfbf7de170c491d6f1e
Edsey omenda <omendajames0317@gmail.com>
2022-10-21 14:33:37 +0300

    creates routes for signing and logging admin

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -1,2 +1,29 @@
 class AdminsController < ApplicationController
+
+    def create
+        admin = Admin.create!(admin_params)
+        if admin.valid?
+            session[:user_id] = admin.id
+            render json: admin,  status: :created
+        else
+            render json: {errors: admin.errors.full_messages}, status: :unprocessable_entity
+        end
+    end
+
+
+
+    def show
+        admin = Admin.find_by(id: session[:admin_id])
+        if admin
+            render json: admin, status: :created
+        else
+            render json: {error: "Not authorized"}, status: :unauthorized
+        end
+    end
+
+    private
+
+    def admin_params
+        params.permit(:user_name, :email, :password)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :admins
+  resources :admins, only: [:index, :show, :create]
   resources :users, only: [:index, :show, :create]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
@@ -10,5 +10,9 @@ Rails.application.routes.draw do
   post '/signup', to: "users#create"
 
   get '/me', to: 'users#show'
+
+  post '/signup', to: 'admins#create'
+
+  get '/me', to: 'admins#show'
 
 end
