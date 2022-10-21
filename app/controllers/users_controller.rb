78fad158540306b918ede142af7a262ae9d8ab61
Edsey omenda <omendajames0317@gmail.com>
2022-10-21 13:47:52 +0300

    creates the login route for the user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,4 +9,19 @@ class UsersController < ApplicationController
             render json: {errors: user.errors.full_messages}, status: :unprocessable_entity
         end
     end
+
+    def show
+        user = User.find_by(id: session[:user_id])
+        if user
+            render json: user, status: :created
+        else
+            render json: {error: "Not authorized"}, status: :unauthorized
+        end
+    end
+
+    private
+
+    def user_params
+        params.permit(:user_name, :email, :password)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,6 @@ Rails.application.routes.draw do
 
   post '/signup', to: "users#create"
 
+  get '/me', to: 'users#show'
+
 end
