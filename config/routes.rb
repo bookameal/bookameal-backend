3d9ae8b9a15d20943bc73cf09032885b320a79be
Edsey omenda <omendajames0317@gmail.com>
2022-10-21 13:37:39 +0300

    creates the user method and updates the routes for a new user

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   resources :admins
-  resources :users
+  resources :users, only: [:index, :show, :create]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+
+  post '/signup', to: "users#create"
+
 end
