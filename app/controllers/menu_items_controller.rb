4cfbd7a4eb3815155aa973889b9966dfb8c20608
Marymuriga <marymuriga@gmail.com>
2022-10-23 23:19:37 +0300

    added post and patch crud

--- app/controllers/menu_items_controller.rb
+++ app/controllers/menu_items_controller.rb
@@ -1,17 +1,40 @@
 class MenuItemsController < ApplicationController
     def index
-        menuitems = MenuItem.all
-        render json: menuitems
+        items = MenuItem.all
+        render json: items
     end
 
     def show 
-        menuitem = find_menuitem
-        render json: menuitem
+        item = find_menuitem
+        render json: item
     end 
+
+    def create 
+        item = MenuItem.create(item_params)
+        render json: item, status: :created
+    end
+
+    def update
+        item = find_menuitem
+        if item
+            item.update(item_params)
+           render json: item
+        else
+            render json: {error: "Item not found"}, status: :not_found
+        end
+    end
 
     private 
     def find_menuitem
         MenuItem.find(params[:id])
     end
+
+    def item_params
+        params.permit(:price, :name,)
+    end 
+
+   
+
+
 
 end
