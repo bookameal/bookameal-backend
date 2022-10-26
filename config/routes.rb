5e7d8dc1ae2a23e9573e6a8be6da8ddc88deefb0
Ivy Ronoh <ronoh.ivy@gmail.com>
2022-10-26 09:00:42 +0300

    Refactored Code

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-<<<<<<< HEAD
+
   resources :menu_items, only: [:index, :show, :create, :update]
   resources :menus, only: [:index]
 
@@ -14,7 +14,6 @@ Rails.application.routes.draw do
 
   get "menus", to: 'menus#index'
 
-=======
 
   resources :users, only: [:index, :show, :create]
   
@@ -33,5 +32,5 @@ Rails.application.routes.draw do
   # put '/menus/:menu_id/update', to: "menus#update_menu"
 
   # delete '/menus/:menu_id/destroy', to: "menus#delete_menus"
->>>>>>> cd811fef11d5e992e2ce992a74e79acb048414c4
+
 end
