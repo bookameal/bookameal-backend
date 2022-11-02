fe2b2eb09927aab2642eca02a4ba212eb26d89f2
Verah Nyagaka <verahnyagaka101@gmail.com>
2022-11-02 22:59:03 +0300

    updated db

--- app/serializers/order_serializer.rb
+++ app/serializers/order_serializer.rb
@@ -1,4 +1,4 @@
 class OrderSerializer < ActiveModel::Serializer
-  attributes :id, :quantity, :dayTime, :user_id, :cart_items
+  attributes :id, :quantity, :dayTime, :user_id, :menu_item_id
   belongs_to :user
 end
